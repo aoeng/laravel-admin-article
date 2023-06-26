658717e8fd0b469a82e78541c77a24928d5aa878
Dang <859726077@qq.com>
2023-06-26 10:00:47 +0800

    fix

--- src/Http/Controllers/ArticleController.php
+++ src/Http/Controllers/ArticleController.php
@@ -33,6 +33,7 @@ class ArticleController extends Controller
             ->select(['id', 'title', 'picture', 'sort', 'created_at'])
             ->where('is_display', 1)
             ->orderByDesc('sort')
+            ->orderByDesc('created_at')
             ->paginate();
 
         return $this->responseJson($article);
